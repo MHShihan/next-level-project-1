c2e28a8d576394683f7fd2ff3c1739f17a79f260
MHShihan <mahmodul.no.1@gmail.com>
2024-05-20 21:59:05 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+// const express = require('express')
+import express, { Request, Response } from "express";
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!!!!!!!");
+});
+
+export default app;
